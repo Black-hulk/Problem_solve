78f1ec7c0168abec57b3084f5598bf7445bf869e
black-hulk <rajibsadhak8@gmail.com>
2023-09-13 23:48:30 +0600

    Array Problem Solve

--- w3resource_arrays_problems_solve/shortest and longest string.php
+++ w3resource_arrays_problems_solve/shortest and longest string.php
@@ -1,4 +1,8 @@
+
 <?php
+/*Write a PHP script to get the shortest/longest string length from an array.
+Sample arrays : ("abcd","abc","de","hjjj","g","wer")
+Expected Output : The shortest array length is 1. The longest array length is 4. */
 $array1 =["abcd","abc","de","hjjj","g","wer"];
 $array_s_length=null;
 $array_l_length=0;
--- w3resource_arrays_problems_solve/shuffle random number.php
+++ w3resource_arrays_problems_solve/shuffle random number.php
@@ -1,4 +1,7 @@
 <?php
+/*Write a PHP script to generate unique random numbers within a range.
+Sample Range : (11, 20)
+Sample Output : 17 16 13 20 14 19 18 15 11 12 */
 $n=range(11,20);
 shuffle($n);
 for($i=0;$i<10;$i++){
